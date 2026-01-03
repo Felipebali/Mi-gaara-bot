d0305f2ab4eb9365c89844d27993c4072fe67ad8
Felipebali <felipebaliski@gmail.com>
2026-01-03 19:23:50 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,29 +1,14 @@
-import { exec } from 'child_process';
-
 let handler = async (m, { conn }) => {
-   m.reply('🔄 ᴀᴄᴛᴜᴀʟɪᴢᴀɴᴅᴏ ʙᴏᴛ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...');
+  await conn.reply(m.chat,
+    '🔄 *Actualizando bot...*\n♻️ Reiniciando servidor...',
+    m
+  )
 
-  exec('git pull', (err, stdout, stderr) => {
-    if (err) {
-      conn.reply(m.chat, `⚠️ Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
-      return;
-    }
+  setTimeout(() => {
+    process.exit(0)
+  }, 1500)
+}
 
-    if (stderr) {
-      console.warn('Advertencia durante la actualización:', stderr);
-    }
-
-    if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, '⚡ ʏᴀ ᴇsᴛᴏʏ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴏ', m);
-    } else {
-      conn.reply(m.chat, `✅ ᴀᴄᴛᴜᴀʟɪᴢᴀᴢɪᴏɴ ᴄᴏɴ ᴇxɪᴛᴏ ᴇᴄʜᴏ\n\n${stdout}`, m);
-    }
-  });
-};
-
-handler.help = ['update'];
-handler.tags = ['owner'];
-handler.command = ['update', 'up', 'fix'];
-handler.rowner = true;
-
-export default handler;
+handler.command = ['update', 'up', 'fix']
+handler.rowner = true
+export default handler
